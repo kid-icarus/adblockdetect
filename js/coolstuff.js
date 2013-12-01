8524050208bd3dbe14f11627ac8355ede8fd310b
Ryan Kois <ryan.kois@gmail.com>
2013-12-01 14:26:23 -0700

    Update.

--- js/coolstuff.js
+++ js/coolstuff.js
@@ -1,14 +1,8 @@
 window.onload = function() {
-  'use strict';
   window.setTimeout(function() {
     var ad = document.getElementById('cooldude');
-    console.log(ad);
-    console.log(typeof ad);
     if (ad) {
-      window.console.log('Dude, you should download ad-block');
-    }
-    else {
-      window.console.log('Good job! You totally have ad-block on');
+      alert('You should download Ad-Block Plus');
     }
   }, 1000);
 };
