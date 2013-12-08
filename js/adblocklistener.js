61aa24f07526e4f0343fca5f99a809e85aeaa9e6
sreynen <scott@slicedbreadlabs.com>
2013-12-08 21:34:07 +0000

    Add enabled listener

--- js/adblocklistener.js
+++ js/adblocklistener.js
@@ -1,5 +1,12 @@
 (function() {
   window.addEventListener('adBlockEnabled', function(e) {
+    var ad = document.getElementById('adblock-detector');
+    var div = document.createElement('div');
+    div.className = 'adblock-alert';
+    var p = document.createElement('p');
+    p.innerHTML = "You're using AdBlock. Great job!";
+    div.appendChild(p);
+    ad.parentNode.appendChild(div);
   });
 
   window.addEventListener('adBlockDisabled', function(e) {
